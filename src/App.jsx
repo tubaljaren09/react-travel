ae66ed0f311c0bc4ae58b6a9bd4772b60c8eae9c
Jaren <tubaljaren@gmail.com>
2023-03-27 17:36:01 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TopBar from "./components/TopBar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <TopBar />
+    </div>
+  );
+};
+
+export default App;
