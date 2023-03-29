32f58a1d9af14f745f1629772147d5ff95257ac9
Jaren <tubaljaren@gmail.com>
2023-03-29 18:19:52 +0800

    Add hero, booking, activities components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import TopBar from "./components/TopBar";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Activities from "./components/Activities";
+import Booking from "./components/Booking";
 
 const App = () => {
   return (
     <div className="App">
       <TopBar />
       <Navbar />
+      <Hero />
+      <Activities />
+      <Booking />
     </div>
   );
 };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="w-full h-[90vh]">
+      <img
+        src="https://images.unsplash.com/photo-1505228395891-9a51e7e86bf6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80"
+        alt="beach"
+        className="w-full h-full object-cover"
+      />
+      <div className="max-w-[1140px] m-auto">
+        <div className="absolute top-[40%] w-full md:top-[50%] max-w-[600px] h-full flex flex-col text-white p-4 drop-shadow-2xl">
+          <h1 className="font-bold text-4xl">Find Your Special Trip</h1>
+          <h2 className="text-4xl py-4 italic">With Weekaway</h2>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
